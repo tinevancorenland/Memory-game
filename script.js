628df467503c67d953288e8f1c7049ab7ea64caf
“[tine <[tinevancorenland@gmail.com]>
2019-06-25 10:53:34 +0200

    initial commit with html & css & starting js

---
+++ script.js
@@ -0,0 +1,13 @@
+//Randomize card positions
+
+//Make cards turn on click
+
+function turnArdeche() {
+  let ardeche = document.getElementById("ardeche");
+  ardeche.classList.toggle("imgArdeche");
+}
+
+function turnArdecheMatch() {
+  let ardecheMatch = document.getElementById("ardecheMatch");
+  ardecheMatch.classList.toggle("imgArdecheMatch");
+}
