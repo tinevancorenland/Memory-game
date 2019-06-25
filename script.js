64690c412dfa49011887ac8f88a2b9d3ae0f01ba
“[tine <[tinevancorenland@gmail.com]>
2019-06-25 14:58:21 +0200

    make the first card toggle classes, now trying to figure out how to make it happen for all cards

--- script.js
+++ script.js
@@ -2,12 +2,35 @@
 
 //Make cards turn on click
 
-function turnArdeche() {
-  let ardeche = document.getElementById("ardeche");
-  ardeche.classList.toggle("imgArdeche");
+// function turnArdeche() {
+//   let ardeche = document.getElementById("ardeche");
+//   ardeche.classList.toggle("imgArdeche");
+// }
+
+// function turnArdecheMatch() {
+//   let ardecheMatch = document.getElementById("ardecheMatch");
+//   ardecheMatch.classList.toggle("imgArdecheMatch");
+// }
+
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+let img = document.getElementsByClassName("cardIMG");
+let imgs = [...img];
+let i;
+
+console.log(cards);
+console.log(imgs);
+
+for (i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", turnAround);
 }
 
-function turnArdecheMatch() {
-  let ardecheMatch = document.getElementById("ardecheMatch");
-  ardecheMatch.classList.toggle("imgArdecheMatch");
+function turnAround() {
+  console.log("hi there");
+}
+
+document.getElementById("ardeche").addEventListener("click", show);
+
+function show() {
+  document.getElementById("ardecheIMG").classList.toggle("showIMG");
 }
