f9bc85008441a21ae85d69ff701675b10a31a04c
“[tine <[tinevancorenland@gmail.com]>
2019-06-27 12:10:01 +0200

    add js flip cards, check for match and flip back if no match

--- script.js
+++ script.js
@@ -1,80 +1,39 @@
-//Randomize card positions
+// https://www.youtube.com/watch?v=ZniVgo8U7ek
+// Make cards flip 
 
-//Make cards turn on click - got stock, to figure out further
+const cards = document.querySelectorAll(".card");
 
-let card = document.getElementsByClassName("card");
-let cards = [...card];
-let img = document.getElementsByClassName("cardIMG");
-let imgs = [...img];
-let i;
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
-console.log(cards);
-console.log(imgs);
+cards.forEach(card => card.addEventListener("click", flipCard));
 
-for (i = 0; i < cards.length; i++) {
-  cards[i].addEventListener("click", turnAround);
+function flipCard() {
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
+  }
 }
 
-function turnAround() {
-  console.log("hi there");
-}
-
-//Make every seperate card turn on click
-//ardeche
-document.getElementById("ardeche").addEventListener("click", showArdeche);
-
-function showArdeche() {
-  document.getElementById("ardecheIMG").classList.toggle("showIMG");
-}
-//bbq
-document.getElementById("bbq").addEventListener("click", showBbq);
-
-function showBbq() {
-  document.getElementById("bbqIMG").classList.toggle("showIMG");
-}
-//roadtrip
-document.getElementById("roadtrip").addEventListener("click", showRoadtrip);
-
-function showRoadtrip() {
-  document.getElementById("roadtripIMG").classList.toggle("showIMG");
-}
-//tent
-document.getElementById("tent").addEventListener("click", showTent);
-
-function showTent() {
-  document.getElementById("tentIMG").classList.toggle("showIMG");
-}
-//ardecheMatch
-document
-  .getElementById("ardecheMatch")
-  .addEventListener("click", showArdecheMatch);
-
-function showArdecheMatch() {
-  document.getElementById("ardecheMatchIMG").classList.toggle("showIMG");
-}
-//bbqMatch
-document.getElementById("bbqMatch").addEventListener("click", showBbqMatch);
-
-function showBbqMatch() {
-  document.getElementById("bbqMatchIMG").classList.toggle("showIMG");
-}
-//roadtripMatch
-document
-  .getElementById("roadtripMatch")
-  .addEventListener("click", showRoadtripMatch);
-
-function showRoadtripMatch() {
-  document.getElementById("roadtripMatchIMG").classList.toggle("showIMG");
-}
-//tentMatch
-document.getElementById("tentMatch").addEventListener("click", showTentMatch);
-
-function showTentMatch() {
-  document.getElementById("tentMatchIMG").classList.toggle("showIMG");
-}
-//joker
-document.getElementById("joker").addEventListener("click", showJoker);
-
-function showJoker() {
-  document.getElementById("jokerIMG").classList.toggle("showIMG");
+function checkForMatch() {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    //it's a match: disable cards
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+  } else {
+    //not a match: unflip cards
+    setTimeout(() => {
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
+    }, 1500);
+  }
 }
